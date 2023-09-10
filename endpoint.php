44627f01868b3d4124b76c054751b1f4c731d670
CodeLord36 <93430616+CodeLord36@users.noreply.github.com>
2023-09-10 14:21:49 +0100

    My First HNG project

---
+++ endpoint.php
@@ -0,0 +1,49 @@
+<?php
+header('Content-Type: application/json');
+
+// Function to validate UTC time within +/-2 minutes
+function validateUtcTime($time) {
+    $currentUtcTime = new DateTime('now', new DateTimeZone('UTC'));
+    $providedTime = new DateTime($time, new DateTimeZone('UTC'));
+
+    $diff = $currentUtcTime->getTimestamp() - $providedTime->getTimestamp();
+
+    return abs($diff) <= 120; // +/- 2 minutes in seconds
+}
+
+// Check if slack_name and track parameters are provided in the URL
+if (isset($_GET['slack_name']) && isset($_GET['track'])) {
+    $slackName = $_GET['slack_name'];
+    $track = $_GET['track'];
+    
+    // Get the current day of the week
+    $currentDay = date('l');
+    
+    // Get the current UTC time
+    $currentUtcTime = gmdate('Y-m-d\TH:i:s\Z');
+    
+    // Define your GitHub repository information
+    $githubFileUrl = 'https://github.com/username/repo/blob/main/file_name.ext';
+    $githubRepoUrl = 'https://github.com/username/repo';
+    
+    // Validate UTC time
+    $isValidUtcTime = validateUtcTime($currentUtcTime);
+    
+    // Prepare the response data
+    $response = [
+        'slack_name' => $slackName,
+        'current_day' => $currentDay,
+        'utc_time' => $isValidUtcTime ? $currentUtcTime : null,
+        'track' => $track,
+        'github_file_url' => $githubFileUrl,
+        'github_repo_url' => $githubRepoUrl,
+        'status_code' => 200,
+    ];
+    
+    // Send the response as JSON
+    echo json_encode($response);
+} else {
+    // If parameters are missing, return an error response
+    http_response_code(400); // Bad Request
+    echo json_encode(['error' => 'Missing slack_name and/or track parameters']);
+}
